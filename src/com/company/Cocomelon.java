50578c78d6bcf4a339787d5ffb32dc9d3906c5bc
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-02 20:16:09 -0600

    added a new comment to the Cocomelon Class

--- src/com/company/Cocomelon.java
+++ src/com/company/Cocomelon.java
@@ -4,5 +4,7 @@ public class Cocomelon {
     public static void main(String[] args) {
         System.out.println("A new class to practice branching");
         //here, we will practice a new branch "feature2"
+        System.out.println("I'm excited to learn Git/GitHub");
+        //we need practice it frequently
     }
 }
