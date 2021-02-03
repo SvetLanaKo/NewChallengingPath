8777e8d765ea50645882c5e1ed83c9a3a6bbcd5d
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-02 21:30:05 -0600

    added a new comment to the 3rd Class

--- src/com/company/CrazyJoke.java
+++ src/com/company/CrazyJoke.java
@@ -2,4 +2,8 @@ package com.company;
 
 public class CrazyJoke {
     //we will practice more branching
+    public static void main(String[] args) {
+        System.out.println("Hi Everyone");
+        //it is needed to be able to work with the team remotely
+    }
 }
