9d49bc31e5c74eed334e1294a3009af7c2d4abf0
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-02 20:14:56 -0600

    created a new Class

---
+++ src/com/company/Cocomelon.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Cocomelon {
+    public static void main(String[] args) {
+        System.out.println("A new class to practice branching");
+        //here, we will practice a new branch "feature2"
+    }
+}
