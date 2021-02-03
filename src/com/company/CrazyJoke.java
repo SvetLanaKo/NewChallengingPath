cc3924229b03f9226b41e56bf3c9fca9779fb474
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-02 21:30:35 -0600

    added a new statement

--- src/com/company/CrazyJoke.java
+++ src/com/company/CrazyJoke.java
@@ -5,5 +5,6 @@ public class CrazyJoke {
     public static void main(String[] args) {
         System.out.println("Hi Everyone");
         //it is needed to be able to work with the team remotely
+        System.out.println("I am happy to learn something new");
     }
 }
